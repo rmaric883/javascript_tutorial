8de8324136760803cf0d2ea7599e37038dd19954
Robert Stabi <rmaric883@gmail.com>
2024-05-10 08:38:33 +0200

    random number generator

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-// COUNTER PROGRAM
-const decreaseBtn = document.getElementById('decreaseBtn');
-const resetBtn = document.getElementById('resetBtn');
-const increaseBtn = document.getElementById('increaseBtn');
-const countLabel = document.getElementById('countLabel');
-let count = 0;
+// RANDOM NUMBER GENERATOR
 
-increaseBtn.onclick = function(){
-    count++;
-    countLabel.textContent = count;
-}
+const myButton = document.getElementById('myButton');
+const label1 = document.getElementById('label1');
+const label2 = document.getElementById('label2');
+const label3 = document.getElementById('label3');
+const min = 1;
+const max = 6;
+let randomNum1;
+let randomNum2;
+let randomNum3;
 
-decreaseBtn.onclick = function(){
-    count--;
-    countLabel.textContent = count;
-}
-
-resetBtn.onclick = function(){
-    count = 0;
-    countLabel.textContent = count;
+myButton.onclick = function(){
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+    label1.textContent = randomNum1;
+    label2.textContent = randomNum2;
+    label3.textContent = randomNum3;
 }
