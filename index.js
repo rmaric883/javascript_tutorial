009342cedbf5db9d7da555e33a72e2947897b396
Robert Stabi <rmaric883@gmail.com>
2024-05-15 09:48:15 +0200

    temperature conversion program

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+// TEMPERATURE CONVERSION PROGRAM
+const textBox = document.getElementById('textBox');
+const toFahrenheit = document.getElementById('toFahrenheit');
+const toCelsius = document.getElementById('toCelsius');
+const result = document.getElementById('result');
 
+function convert() {
+    if (toFahrenheit.checked) {
+        temp = Number(textBox.value);
+        temp = (9 / 5 * temp) + 32;
+        result.textContent = `${temp.toFixed(1)} °F`
+    } else if (toCelsius.checked) {
+        temp = Number(textBox.value);
+        temp = 5 / 9 * (temp -32);
+        result.textContent = `${temp.toFixed(1)} °C`
+    } else {
+        result.textContent = 'Select a unit'
+    }
+}
