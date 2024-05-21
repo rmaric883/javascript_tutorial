7d7bca9afb98c1d1caf80ca78efc044997e2e83d
Robert Stabi <rmaric883@gmail.com>
2024-05-21 10:39:58 +0200

    digital clock program

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+function updateClock() {
+    const now = new Date();
+    let hours = now.getHours();
+    const meridiem = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12 || 12;
+    hours = hours.toString().padEnd(2,0)
+    const minutes = now.getMinutes().toString().padEnd(2,0);
+    const seconds = now.getSeconds().toString().padEnd(2,0);
+    const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
+    document.getElementById('clock').textContent = timeString;
+}
+
+updateClock();
+
+
+
+setInterval(updateClock, 1000)
