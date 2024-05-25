cc601b9b58d569934188ffa66f50a7684abb472b
Robert Stabi <rmaric883@gmail.com>
2024-05-25 14:21:44 +0200

    event-listeners

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const myBox = document.getElementById('myBox');
+const myButton = document.getElementById('myButton');
+
+myButton.addEventListener('click', event => {
+    myBox.style.backgroundColor = 'tomato';
+    myBox.textContent = 'OUCH! 😳';
+});
+
+myButton.addEventListener('mouseover', event => {
+    myButton.style.cursor = 'pointer';
+    myBox.style.backgroundColor = 'yellow';
+    myBox.textContent = "Don't do it! 😱";
+})
+
+myButton.addEventListener('mouseout', event => {
+    myButton.style.cursor = 'normal';
+    myBox.style.backgroundColor = 'lightgreen';
+    myBox.textContent = 'Click Me 😃';
+})
 
